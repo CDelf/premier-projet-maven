03af060def83dc4463437064db2b3d5fa46b40f1
Carole <c.delfino@live.fr>
2025-04-04 16:03:05 +0200

    Tp1 - Etapes 3 à 7

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -3,8 +3,11 @@ package dev;
 /**
  * Hello world!
  */
+import com.github.lalyos.jfiglet.FigletFont;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String asciiArt = FigletFont.convertOneLine("Hello World!");
+        System.out.println(asciiArt);
     }
 }
