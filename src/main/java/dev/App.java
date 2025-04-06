5d26c5595bc228bb0fb10db3831dd4b5ff5ec6e2
Carole <c.delfino@live.fr>
2025-04-06 16:48:06 +0200

    Tp3 - profils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -6,12 +6,19 @@ package dev;
 import com.github.lalyos.jfiglet.FigletFont;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class App {
     public static void main(String[] args) {
         try {
-            String asciiArt = FigletFont.convertOneLine("Hello World!");
+            String titre = ResourceBundle.getBundle("application").getString("titre");
+            String asciiArt = FigletFont.convertOneLine(titre);
             System.out.println(asciiArt);
+
+            // affichage de l'environnement
+            String environnement = ResourceBundle.getBundle("application").getString("environnement");
+            System.out.println("Environnement : " + environnement);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
